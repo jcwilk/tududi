d2ebbe06e08cada6ac321d2e9880293b2d8fae8e
John Wilkinson <me@jcwilk.com>
2024-10-13 22:05:59 -0700

    fix deprecation

--- app/models/task.rb
+++ app/models/task.rb
@@ -3,8 +3,8 @@ class Task < ActiveRecord::Base
   belongs_to :project, optional: true
   has_and_belongs_to_many :tags
 
-  enum priority: { low: 0, medium: 1, high: 2 }
-  enum status: { not_started: 0, in_progress: 1, done: 2, archived: 3 }
+  enum :priority, { low: 0, medium: 1, high: 2 }
+  enum :status, { not_started: 0, in_progress: 1, done: 2, archived: 3 }
 
   scope :complete, -> { where(status: statuses[:done]) }
   scope :incomplete, -> { where.not(status: statuses[:done]) }
